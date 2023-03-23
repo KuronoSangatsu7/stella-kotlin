01ae16e55b2e74469828d1e1264d6b30131ffb74
Jaffar Totanji <94690848+KuronoSangatsu7@users.noreply.github.com>
2023-03-23 15:09:17 +0300

    Initial commit

--- src/main/kotlin/org/stella/typecheck/TypeCheck.kt
+++ src/main/kotlin/org/stella/typecheck/TypeCheck.kt
@@ -1,24 +1,11 @@
 package org.stella.typecheck
 
-import org.syntax.stella.Absyn.AProgram
-import org.syntax.stella.Absyn.DeclFun
-import org.syntax.stella.Absyn.DeclTypeAlias
 import org.syntax.stella.Absyn.Program
 
 object TypeCheck {
     @Throws(Exception::class)
-    fun typecheckProgram(program: Program) {
-        when (program) {
-            is AProgram -> program.listdecl_.map {
-                when (it) {
-                    is DeclFun -> {
-                        val name = it.stellaident_
-                        println("Declared function $name")
-                    }
-                    is DeclTypeAlias -> {}
-                }
-            }
-
-        }
+    fun typecheckProgram(program: Program?) {
+        println("typechecker is not implemented!")
+        return
     }
 }
